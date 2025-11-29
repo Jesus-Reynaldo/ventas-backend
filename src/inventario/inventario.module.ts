cb756866da6244320944edd5d8dd6ed5fdcfd30a
FrankSlaz <henryfrank96@hotmail.com>
2025-11-29 03:52:02 -0400

    Exportar Inventario

--- src/inventario/inventario.module.ts
+++ src/inventario/inventario.module.ts
@@ -2,10 +2,11 @@
 import { Module } from '@nestjs/common';
 import { InventarioService } from './inventario.service';
 import { InventarioController } from './inventario.controller';
+import { PdfService } from './pdf.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [InventarioController],
-  providers: [InventarioService, PrismaService],
+  providers: [InventarioService, PdfService, PrismaService],
 })
 export class InventarioModule {}
